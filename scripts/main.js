759bb8600468b34e19281144c445c866a40792a4
Matt Heerspink <mheerspink75@gmail.com>
2019-08-15 14:58:34 -0500

    added JQuery section

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+//Working JavaScript Code
+
 const gifForm = document.querySelector("#gif-form");
 gifForm.addEventListener("submit", fetchGiphs);
 
@@ -28,3 +30,31 @@ function showGiphs(dataArray) {
 
   document.querySelector(".results").innerHTML = output;
 }
+
+
+//Working jQuery Code
+//The top jQuery Code is working
+/*
+const gifForm = $("#gif-form");
+gifForm.submit(e => {
+  e.preventDefault();
+  const searchTerm = $('.search').val();
+  const url = `https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=3mIxmBZUIIPyb8R69gtxaW8Hsh74dFKV`;
+
+  $.get(url)
+    .done(resp => {
+      showGiphys(resp.data.slice(0, 80));
+    }).fail(console.log);
+});
+*/
+//This jQuery Code is not working
+/*
+function showGiphys(dataArray) {
+  const resultBody = dataArray
+    .map(imgData => `<a href="${imgData.images.original.url}" alt="${imgData.title}" target="_blank"><img src="${imgData.images.original.url}"></a>`)
+    .join('');
+  $(".results").innerHTML = `<div class="results">${resultBody}</div>`;
+}
+*/
+
+
