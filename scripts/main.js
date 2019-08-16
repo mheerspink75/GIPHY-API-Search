945366d0d349ffe8fae4fce6eba57ea1d9735884
Matt Heerspink <mheerspink75@gmail.com>
2019-08-16 16:46:33 -0500

    updated jQuery output to let output = '';

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,5 @@
-//Working JavaScript Code
-
-const gifForm = document.querySelector("#gif-form");
-gifForm.addEventListener("submit", fetchGiphs);
-
-function fetchGiphs(e) {
-  e.preventDefault();
-  const searchTerm = document.querySelector(".search").value;
-
-  fetch(`https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=3mIxmBZUIIPyb8R69gtxaW8Hsh74dFKV`)
-    .then(response => {
-      return response.json();
-    })
-    .then(resp => {
-      console.log(resp);
-      let dataArray = resp.data;
-      showGiphs(dataArray);
-    })
-    .catch(err => console.log(err));
-}
-
-function showGiphs(dataArray) {
-  const results = document.querySelector(".results");
-  let output = '<div class="results">';
-
-  dataArray.forEach(imgData => {
-    output += `<a href="${imgData.images.original.url}" alt="${imgData.title}" target="_blank"><img src="${imgData.images.original.url}"></a>`;
-
-  });
-
-  document.querySelector(".results").innerHTML = output;
-}
-
-
 //Working jQuery Code
 //The top jQuery Code is working
-/*
 const gifForm = $("#gif-form");
 gifForm.submit(e => {
   e.preventDefault();
@@ -43,13 +8,42 @@ gifForm.submit(e => {
 
   $.get(url)
     .done(resp => {
-      showGiphys(resp.data.slice(0, 80));
+      showGiphs(resp.data.slice(0, 80));
     }).fail(console.log);
 });
+
+//Working JavaScript Code
+function showGiphs(dataArray) {
+  const results = document.querySelector(".results");
+  let output = '';
+  dataArray.forEach(imgData => {
+    output += `<a href="${imgData.images.original.url}" alt="${imgData.title}" target="_blank"><img src="${imgData.images.original.url}"></a>`;
+  });
+  document.querySelector(".results").innerHTML = output;
+}
+
+//Working JavaScript Code
+/*
+const gifForm = document.querySelector("#gif-form");
+gifForm.addEventListener("submit", fetchGiphs);
+
+function fetchGiphs(e) {
+  e.preventDefault();
+  const searchTerm = document.querySelector(".search").value;
+
+  fetch(`https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=99&api_key=3mIxmBZUIIPyb8R69gtxaW8Hsh74dFKV`)
+    .then(response => response.json())
+    .then(resp => {
+      let dataArray = resp.data.slice(0, 80);
+      showGiphs(dataArray);
+    })
+    .catch(console.error);
+}
 */
+
 //This jQuery Code is not working
 /*
-function showGiphys(dataArray) {
+function showGiphs(dataArray) {
   const resultBody = dataArray
     .map(imgData => `<a href="${imgData.images.original.url}" alt="${imgData.title}" target="_blank"><img src="${imgData.images.original.url}"></a>`)
     .join('');
